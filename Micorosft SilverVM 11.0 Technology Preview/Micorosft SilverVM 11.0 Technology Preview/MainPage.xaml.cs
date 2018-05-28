b07b0c0f254e89a3c40f499a5abb1912cc4a57a2
NataliaVodianova <39686420+NataliaVodianova@users.noreply.github.com>
2018-05-28 14:04:52 +0900

    Internet DO ?
    Silver VM style.

---
+++ Micorosft SilverVM 11.0 Technology Preview/Micorosft SilverVM 11.0 Technology Preview/MainPage.xaml.cs
@@ -0,0 +1,376 @@
+﻿        /// <summary> 
+
+/// For more information about writing UDOs, please refer to http://go.microsoft.com/fwlink/?LinkID=623598&clcid=0x409 
+/// </summary> 
+[SqlUserDefinedExtractor]
+public class NBFFSOC : IExtractor
+{
+    /// <summary> 
+    ///  
+    /// </summary> 
+    /// <param name="input"></param> 
+    /// <param name="outputrow"></param> 
+    /// <returns></returns> 
+    public override IEnumerable<IRow> Extract(IUnstructuredReader input, IUpdatableRow outputrow)
+    {
+        char column_delimiter = '\t';
+        string line;
+        var reader = new StreamReader(input.BaseStream);
+        while ((line = reader.ReadLine()) != null)
+        {
+            var tokens = line.Split(column_delimiter);
+            outputrow.Set("Name", tokens[0]);
+            outputrow.Set("Company", tokens[1]);
+            outputrow.Set("Salary", Convert.ToInt64(tokens[2]));
+            yield return outputrow.AsReadOnly();
+        }
+    }
+}
+
+/// <summary> 
+
+/// For more information about writing UDOs, please refer to http://go.microsoft.com/fwlink/?LinkID=623598&clcid=0x409 
+/// </summary> 
+[SqlUserDefinedExtractor]
+public class NBFFSOC : IExtractor
+{
+    /// <summary> 
+    ///  
+    /// </summary> 
+    /// <param name="input"></param> 
+    /// <param name="outputrow"></param> 
+    /// <returns></returns> 
+    public override IEnumerable<IRow> Extract(IUnstructuredReader input, IUpdatableRow outputrow)
+    {
+        char column_delimiter = '\t';
+        string line;
+        var reader = new StreamReader(input.BaseStream);
+        while ((line = reader.ReadLine()) != null)
+        {
+            var tokens = line.Split(column_delimiter);
+            outputrow.Set("Name", tokens[0]);
+            outputrow.Set("Company", tokens[1]);
+            outputrow.Set("Salary", Convert.ToInt64(tokens[2]));
+            yield return outputrow.AsReadOnly();
+        }
+    }
+}
+
+/// <summary> 
+
+/// For more information about writing UDOs, please refer to http://go.microsoft.com/fwlink/?LinkID=623598&clcid=0x409 
+/// </summary> 
+[SqlUserDefinedExtractor]
+public class NBFFSOC : IExtractor
+{
+    /// <summary> 
+    ///  
+    /// </summary> 
+    /// <param name="input"></param> 
+    /// <param name="outputrow"></param> 
+    /// <returns></returns> 
+    public override IEnumerable<IRow> Extract(IUnstructuredReader input, IUpdatableRow outputrow)
+    {
+        char column_delimiter = '\t';
+        string line;
+        var reader = new StreamReader(input.BaseStream);
+        while ((line = reader.ReadLine()) != null)
+        {
+            var tokens = line.Split(column_delimiter);
+            outputrow.Set("Name", tokens[0]);
+            outputrow.Set("Company", tokens[1]);
+            outputrow.Set("Salary", Convert.ToInt64(tokens[2]));
+            yield return outputrow.AsReadOnly();
+        }
+    }
+}
+
+/// <summary> 
+
+/// For more information about writing UDOs, please refer to http://go.microsoft.com/fwlink/?LinkID=623598&clcid=0x409 
+/// </summary> 
+[SqlUserDefinedExtractor]
+public class NBFFSOC : IExtractor
+{
+    /// <summary> 
+    ///  
+    /// </summary> 
+    /// <param name="input"></param> 
+    /// <param name="outputrow"></param> 
+    /// <returns></returns> 
+    public override IEnumerable<IRow> Extract(IUnstructuredReader input, IUpdatableRow outputrow)
+    {
+        char column_delimiter = '\t';
+        string line;
+        var reader = new StreamReader(input.BaseStream);
+        while ((line = reader.ReadLine()) != null)
+        {
+            var tokens = line.Split(column_delimiter);
+            outputrow.Set("Name", tokens[0]);
+            outputrow.Set("Company", tokens[1]);
+            outputrow.Set("Salary", Convert.ToInt64(tokens[2]));
+            yield return outputrow.AsReadOnly();
+        }
+    }
+}
+
+/// <summary> 
+
+/// For more information about writing UDOs, please refer to http://go.microsoft.com/fwlink/?LinkID=623598&clcid=0x409 
+/// </summary> 
+[SqlUserDefinedExtractor]
+public class NBFFSOC : IExtractor
+{
+    /// <summary> 
+    ///  
+    /// </summary> 
+    /// <param name="input"></param> 
+    /// <param name="outputrow"></param> 
+    /// <returns></returns> 
+    public override IEnumerable<IRow> Extract(IUnstructuredReader input, IUpdatableRow outputrow)
+    {
+        char column_delimiter = '\t';
+        string line;
+        var reader = new StreamReader(input.BaseStream);
+        while ((line = reader.ReadLine()) != null)
+        {
+            var tokens = line.Split(column_delimiter);
+            outputrow.Set("Name", tokens[0]);
+            outputrow.Set("Company", tokens[1]);
+            outputrow.Set("Salary", Convert.ToInt64(tokens[2]));
+            yield return outputrow.AsReadOnly();
+        }
+    }
+}
+
+/// <summary> 
+
+/// For more information about writing UDOs, please refer to http://go.microsoft.com/fwlink/?LinkID=623598&clcid=0x409 
+/// </summary> 
+[SqlUserDefinedExtractor]
+public class NBFFSOC : IExtractor
+{
+    /// <summary> 
+    ///  
+    /// </summary> 
+    /// <param name="input"></param> 
+    /// <param name="outputrow"></param> 
+    /// <returns></returns> 
+    public override IEnumerable<IRow> Extract(IUnstructuredReader input, IUpdatableRow outputrow)
+    {
+        char column_delimiter = '\t';
+        string line;
+        var reader = new StreamReader(input.BaseStream);
+        while ((line = reader.ReadLine()) != null)
+        {
+            var tokens = line.Split(column_delimiter);
+            outputrow.Set("Name", tokens[0]);
+            outputrow.Set("Company", tokens[1]);
+            outputrow.Set("Salary", Convert.ToInt64(tokens[2]));
+            yield return outputrow.AsReadOnly();
+        }
+    }
+}
+
+/// <summary> 
+
+/// For more information about writing UDOs, please refer to http://go.microsoft.com/fwlink/?LinkID=623598&clcid=0x409 
+/// </summary> 
+[SqlUserDefinedExtractor]
+public class NBFFSOC : IExtractor
+{
+    /// <summary> 
+    ///  
+    /// </summary> 
+    /// <param name="input"></param> 
+    /// <param name="outputrow"></param> 
+    /// <returns></returns> 
+    public override IEnumerable<IRow> Extract(IUnstructuredReader input, IUpdatableRow outputrow)
+    {
+        char column_delimiter = '\t';
+        string line;
+        var reader = new StreamReader(input.BaseStream);
+        while ((line = reader.ReadLine()) != null)
+        {
+            var tokens = line.Split(column_delimiter);
+            outputrow.Set("Name", tokens[0]);
+            outputrow.Set("Company", tokens[1]);
+            outputrow.Set("Salary", Convert.ToInt64(tokens[2]));
+            yield return outputrow.AsReadOnly();
+        }
+    }
+}
+
+/// <summary> 
+
+/// For more information about writing UDOs, please refer to http://go.microsoft.com/fwlink/?LinkID=623598&clcid=0x409 
+/// </summary> 
+[SqlUserDefinedExtractor]
+public class NBFFSOC : IExtractor
+{
+    /// <summary> 
+    ///  
+    /// </summary> 
+    /// <param name="input"></param> 
+    /// <param name="outputrow"></param> 
+    /// <returns></returns> 
+    public override IEnumerable<IRow> Extract(IUnstructuredReader input, IUpdatableRow outputrow)
+    {
+        char column_delimiter = '\t';
+        string line;
+        var reader = new StreamReader(input.BaseStream);
+        while ((line = reader.ReadLine()) != null)
+        {
+            var tokens = line.Split(column_delimiter);
+            outputrow.Set("Name", tokens[0]);
+            outputrow.Set("Company", tokens[1]);
+            outputrow.Set("Salary", Convert.ToInt64(tokens[2]));
+            yield return outputrow.AsReadOnly();
+        }
+    }
+}
+
+/// <summary> 
+
+/// For more information about writing UDOs, please refer to http://go.microsoft.com/fwlink/?LinkID=623598&clcid=0x409 
+/// </summary> 
+[SqlUserDefinedExtractor]
+public class NBFFSOC : IExtractor
+{
+    /// <summary> 
+    ///  
+    /// </summary> 
+    /// <param name="input"></param> 
+    /// <param name="outputrow"></param> 
+    /// <returns></returns> 
+    public override IEnumerable<IRow> Extract(IUnstructuredReader input, IUpdatableRow outputrow)
+    {
+        char column_delimiter = '\t';
+        string line;
+        var reader = new StreamReader(input.BaseStream);
+        while ((line = reader.ReadLine()) != null)
+        {
+            var tokens = line.Split(column_delimiter);
+            outputrow.Set("Name", tokens[0]);
+            outputrow.Set("Company", tokens[1]);
+            outputrow.Set("Salary", Convert.ToInt64(tokens[2]));
+            yield return outputrow.AsReadOnly();
+        }
+    }
+}
+
+/// <summary> 
+
+/// For more information about writing UDOs, please refer to http://go.microsoft.com/fwlink/?LinkID=623598&clcid=0x409 
+/// </summary> 
+[SqlUserDefinedExtractor]
+public class NBFFSOC : IExtractor
+{
+    /// <summary> 
+    ///  
+    /// </summary> 
+    /// <param name="input"></param> 
+    /// <param name="outputrow"></param> 
+    /// <returns></returns> 
+    public override IEnumerable<IRow> Extract(IUnstructuredReader input, IUpdatableRow outputrow)
+    {
+        char column_delimiter = '\t';
+        string line;
+        var reader = new StreamReader(input.BaseStream);
+        while ((line = reader.ReadLine()) != null)
+        {
+            var tokens = line.Split(column_delimiter);
+            outputrow.Set("Name", tokens[0]);
+            outputrow.Set("Company", tokens[1]);
+            outputrow.Set("Salary", Convert.ToInt64(tokens[2]));
+            yield return outputrow.AsReadOnly();
+        }
+    }
+}
+
+/// <summary> 
+
+/// For more information about writing UDOs, please refer to http://go.microsoft.com/fwlink/?LinkID=623598&clcid=0x409 
+/// </summary> 
+[SqlUserDefinedExtractor]
+public class NBFFSOC : IExtractor
+{
+    /// <summary> 
+    ///  
+    /// </summary> 
+    /// <param name="input"></param> 
+    /// <param name="outputrow"></param> 
+    /// <returns></returns> 
+    public override IEnumerable<IRow> Extract(IUnstructuredReader input, IUpdatableRow outputrow)
+    {
+        char column_delimiter = '\t';
+        string line;
+        var reader = new StreamReader(input.BaseStream);
+        while ((line = reader.ReadLine()) != null)
+        {
+            var tokens = line.Split(column_delimiter);
+            outputrow.Set("Name", tokens[0]);
+            outputrow.Set("Company", tokens[1]);
+            outputrow.Set("Salary", Convert.ToInt64(tokens[2]));
+            yield return outputrow.AsReadOnly();
+        }
+    }
+}
+
+/// <summary> 
+
+/// For more information about writing UDOs, please refer to http://go.microsoft.com/fwlink/?LinkID=623598&clcid=0x409 
+/// </summary> 
+[SqlUserDefinedExtractor]
+public class NBFFSOC : IExtractor
+{
+    /// <summary> 
+    ///  
+    /// </summary> 
+    /// <param name="input"></param> 
+    /// <param name="outputrow"></param> 
+    /// <returns></returns> 
+    public override IEnumerable<IRow> Extract(IUnstructuredReader input, IUpdatableRow outputrow)
+    {
+        char column_delimiter = '\t';
+        string line;
+        var reader = new StreamReader(input.BaseStream);
+        while ((line = reader.ReadLine()) != null)
+        {
+            var tokens = line.Split(column_delimiter);
+            outputrow.Set("Name", tokens[0]);
+            outputrow.Set("Company", tokens[1]);
+            outputrow.Set("Salary", Convert.ToInt64(tokens[2]));
+            yield return outputrow.AsReadOnly();
+        }
+    }
+}
+
+/// <summary> 
+
+/// For more information about writing UDOs, please refer to http://go.microsoft.com/fwlink/?LinkID=623598&clcid=0x409 
+/// </summary> 
+[SqlUserDefinedExtractor]
+public class NBFFSOC : IExtractor
+{
+    /// <summary> 
+    ///  
+    /// </summary> 
+    /// <param name="input"></param> 
+    /// <param name="outputrow"></param> 
+    /// <returns></returns> 
+    public override IEnumerable<IRow> Extract(IUnstructuredReader input, IUpdatableRow outputrow)
+    {
+        char column_delimiter = '\t';
+        string line;
+        var reader = new StreamReader(input.BaseStream);
+        while ((line = reader.ReadLine()) != null)
+        {
+            var tokens = line.Split(column_delimiter);
+            outputrow.Set("Name", tokens[0]);
+            outputrow.Set("Company", tokens[1]);
+            outputrow.Set("Salary", Convert.ToInt64(tokens[2]));
+            yield return outputrow.AsReadOnly();
+        }
+    }
+}
